3bc3e4e3747e815674fbc0f1777a9f8a37239856
Mohammed Jabir <med.jabir@gmail.com>
2019-10-16 19:57:39 +0100

    modifyih master 3icha 3abd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    <h1>WELCOME TO Dossor</h1>
+                    <h1>WELCOME TO Tigmi n 3icha 3aaaab</h1>
                 </div>
             </div>
         </div>
